fca880492235c5cf739a65483febc4f135f7b3a7
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-26 14:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const select=document.getElementById("colorSelect");
+
+select.addEventListener("click", onClickRemove);
+
+function onClickRemove(event) {
+
+	let selectValue=select.value;
+	let option=select.children;
+	for(let i=0;i<option.length;i++){
+		if(option[i].innerText===selectValue){
+			option[i].remove();
+			break;
+		}
+	}
+}
