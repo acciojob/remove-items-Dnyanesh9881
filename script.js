9122c0f8fcb08cc4a7b90e6529a8d5009393162a
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-26 14:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 const select=document.getElementById("colorSelect");
-
-select.addEventListener("click", onClickRemove);
+const input=document.querySelector("input");
+input.addEventListener("click", onClickRemove);
 
 function onClickRemove(event) {
 
 	let selectValue=select.value;
 	let option=select.children;
 	for(let i=0;i<option.length;i++){
-		if(option[i].innerText===selectValue){
+		if(option[i].value===selectValue){
 			option[i].remove();
 			break;
 		}
